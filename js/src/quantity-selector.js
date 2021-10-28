848a617b5216629ee3da6b4595076e20d30fb025
MewenLeHo <mewen.leho.ext@orange.com>
2021-10-28 15:17:08 +0200

    feat #113 (quantity selector): create new component

--- js/src/quantity-selector.js
+++ js/src/quantity-selector.js
@@ -6,6 +6,7 @@
  */
 
 import { defineJQueryPlugin } from './util/index'
+import EventHandler from './dom/event-handler'
 import BaseComponent from './base-component'
 
 /**
@@ -14,10 +15,20 @@ import BaseComponent from './base-component'
 * ------------------------------------------------------------------------
 */
 
-const NAME = 'QuantitySelector'
+const NAME = 'quantityselector'
 
-const STEP_UP_BUTTON = document.querySelectorAll('[data-bs-step=\'up\']')
-const STEP_DOWN_BUTTON = document.querySelectorAll('[data-bs-step=\'down\']')
+const DATA_KEY = 'bs.quantityselector'
+const EVENT_KEY = `.${DATA_KEY}`
+const DATA_API_KEY = '.data-api'
+
+const EVENT_CLICK_DATA_API = `click${EVENT_KEY}${DATA_API_KEY}`
+
+// const STEP_UP_BUTTON = document.querySelectorAll('[data-bs-step=\'up\']')
+// const STEP_DOWN_BUTTON = document.querySelectorAll('[data-bs-step=\'down\']')
+
+const STEP_UP_BUTTON = '[data-bs-step="up"]'
+const STEP_DOWN_BUTTON = '[data-bs-step="down"]'
+const INPUT_GROUP = '.input-group'
 
 /**
 * ------------------------------------------------------------------------
@@ -32,29 +43,29 @@ class QuantitySelector extends BaseComponent {
   }
 
   // Public
-
-  step_up() {
-    for (const BUTTON of STEP_UP_BUTTON) {
-      BUTTON.addEventListener('click', () => {
-        console.log('up')
-        const PARENT = BUTTON.closest('.input-group')
-        const COUNTER_INPUT = PARENT.querySelector('[data-bs-step=\'counter\']')
-        COUNTER_INPUT.value = ++COUNTER_INPUT.value
-      })
-    }
+  static StepUp(event) {
+      event.preventDefault()
+      const PARENT = event.target.closest(INPUT_GROUP)
+      const COUNTER_INPUT = PARENT.querySelector('[data-bs-step=\'counter\']')
+      COUNTER_INPUT.value = ++COUNTER_INPUT.value
   }
 
-  step_down() {
-    for (const BUTTON of STEP_DOWN_BUTTON) {
-      BUTTON.addEventListener('click', () => {
-        console.log('down')
-        const PARENT = BUTTON.closest('.input-group')
-        const COUNTER_INPUT = PARENT.querySelector('[data-bs-step=\'counter\']')
-        COUNTER_INPUT.value = --COUNTER_INPUT.value
-      })
-    }
+  static StepDown(event) {
+      event.preventDefault()
+      const PARENT = event.target.closest(INPUT_GROUP)
+      const COUNTER_INPUT = PARENT.querySelector('[data-bs-step=\'counter\']')
+      COUNTER_INPUT.value = --COUNTER_INPUT.value
   }
 }
+
+/**
+ * ------------------------------------------------------------------------
+ * Data Api implementation
+ * ------------------------------------------------------------------------
+ */
+
+EventHandler.on(document, EVENT_CLICK_DATA_API, STEP_UP_BUTTON, QuantitySelector.StepUp)
+EventHandler.on(document, EVENT_CLICK_DATA_API, STEP_DOWN_BUTTON, QuantitySelector.StepDown)
 
 /**
 * ------------------------------------------------------------------------
